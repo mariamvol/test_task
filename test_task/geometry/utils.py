f7d5c5a73119872d8e47e754ccebee4c7229fd38
maria <146164676+mariamvol@users.noreply.github.com>
2025-09-14 23:09:39 +0300

    Delete test_task/geometry directory

--- test_task/geometry/utils.py
+++
@@ -1,9 +0,0 @@
-#utils.py
-from .base import Shape
-
-
-def compute_area(shape: Shape) -> float:
-    """Полиморфный расчeт площади фигуры без знания еe типа в compile-time"""
-    return shape.area()
-
-
